3f03a1025287bc3f468392f671276065be466afd
fatimafurqan07 <fatima.furqan321@gmail.com>
2026-08-18 00:57:31 +0500

    landing page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +8,48 @@ namespace GiveAID_Project.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: / or /Home/Index
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        // GET: /Home/About
+        [HttpGet]
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
+        // GET: /Home/Partners
+        [HttpGet]
+        public ActionResult Partners()
+        {
+            return View();
+        }
+
+        // GET: /Home/Contact
+        [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        // POST: /Home/Contact
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(string name, string email, string subject, string message, string inquirerType)
+        {
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(message))
+            {
+                TempData["ErrorMessage"] = "Please fill in all required fields (Name, Email, and Message).";
+                return View();
+            }
+
+            // Successfully received inquiry (ready for future email/DB backend integration)
+            TempData["SuccessMessage"] = $"Thank you, {name}! Your message has been received. Our team will get back to you at {email} within 24 hours.";
+            return RedirectToAction("Contact");
         }
     }
 }
